594963876677cabe0d16d0b16a867f21b953d8e9
Charlotte McCanna <charliegmccanna@gmail.com>
2022-06-28 06:13:39 +0000

    Completed node-fs-readfile.

---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+fs.readFile((process.argv[process.argv.length - 1]), 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
